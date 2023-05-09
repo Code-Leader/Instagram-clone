ea8396080366a07173881467efb28e61a546e9da
Code-Leader <islomov.islombek.dev@gmail.com>
2023-05-09 23:29:31 +0500

    I'm created shared app and models

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
     # local apps
     'users',
+    'shared',
 ]
 
 MIDDLEWARE = [
@@ -114,8 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
---
+++ shared/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+import uuid
+
+class BaseModel(models.Model):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False, primary_key=True)
+    cretad_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class META:
+        abstract = True
